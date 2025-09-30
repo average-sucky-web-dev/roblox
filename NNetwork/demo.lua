20d255f52b1d789d78d06c97ca6d32da9699c11c
average-sucky-web-dev <144091957+average-sucky-web-dev@users.noreply.github.com>
2025-09-30 22:09:22 +1000

    Create demo.lua

---
+++ NNetwork/demo.lua
@@ -0,0 +1,34 @@
+local NNetwork = require(script.Parent.NNetwork)
+
+local AI = NNetwork.new({4, 10, 4})
+
+local function createDataset(n)
+	local dataset = {}
+	for i = 1, n do
+		local x = {}
+		local sum = 0
+		for j = 1, 4 do
+			local val = math.random()
+			table.insert(x, {val})
+			sum = sum + val
+		end
+		local mean = sum / 4
+		local y = {}
+		if mean < 0.25 then
+			y = {{1},{0},{0},{0}}
+		elseif mean < 0.5 then
+			y = {{0},{1},{0},{0}}
+		elseif mean < 0.75 then
+			y = {{0},{0},{1},{0}}
+		else
+			y = {{0},{0},{0},{1}}
+		end
+		table.insert(dataset, {x, y})
+	end
+	return dataset
+end
+
+NNetwork.load()
+local dataset = createDataset(100)
+print(AI:train(dataset, 0.1, 200))
+print(AI:predict({0.4, 0.2, 1, 0.35}))
